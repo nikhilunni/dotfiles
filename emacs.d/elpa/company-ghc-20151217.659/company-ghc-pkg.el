983dd90ff32c1a50403c77f77209ba0e4a8ee4b8
Nikhil Unni <nunni2@illinois.edu>
2015-12-20 16:04:58 -0800

    company-gc files for emacs

---
+++ emacs.d/elpa/company-ghc-20151217.659/company-ghc-pkg.el
@@ -0,0 +1,1 @@
+(define-package "company-ghc" "20151217.659" "company-mode ghc-mod backend" (quote ((cl-lib "0.5") (company "0.8.0") (ghc "5.4.0.0") (emacs "24"))))
---
+++ emacs.d/elpa/company-ghci-20151206.1050/company-ghci-autoloads.el
@@ -0,0 +1,29 @@
+;;; company-ghci-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (company-ghci) "company-ghci" "company-ghci.el"
+;;;;;;  (22135 11205 0 0))
+;;; Generated autoloads from company-ghci.el
+
+(autoload 'company-ghci "company-ghci" "\
+Company backend that provides completions using the current ghci process.
+
+\(fn COMMAND &optional ARG &rest IGNORED)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("company-ghci-pkg.el") (22135 11205 650417
+;;;;;;  0))
+
+;;;***
+
+(provide 'company-ghci-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; company-ghci-autoloads.el ends here
